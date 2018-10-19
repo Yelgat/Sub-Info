9335a7e3bd9c84a95d4b7e3cbeef5c0cd094affa
hifromkevin <lam.kevin86@gmail.com>
2018-10-19 08:14:10 -0700

    Adds Fontawesome

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import FontAwesome from 'react-fontawesome';
 
 import App from './components/App.jsx';
 import '../dist/styles/style.sass';
-
 import 'font-awesome/css/font-awesome.css';
-import FontAwesome from 'react-fontawesome';
 
 ReactDOM.render(<App />, document.getElementById('app'));
